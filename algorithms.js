887edcb50d7d919d9e0f5253938bb7c544416590
Mendel Cohen <mmhakohen@gmail.com>
2021-04-12 13:16:46 -0400

    solves collatz conjectue and largest palindromic number

--- algorithms.js
+++ algorithms.js
@@ -431,4 +431,85 @@ const fibonacci = num => {
   }
   return array[num - 1];
 };
-console.log(fibonacci(13));
+// console.log(fibonacci(13));
+
+
+
+
+// If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+// Find the sum of all the multiples of 3 or 5 below 1000.
+
+function addMultiples(num) {
+  var sum = 0;
+  for (var i = 1; i <= 1000; i++) {
+    if (i % 3 === 0 || i % 5 === 0) {
+      sum += i;
+    } 
+  }
+  return sum;
+}
+// console.log(addMultiples(1000));
+
+
+
+// The Collatz Conjecture or 3x+1 problem can be summarized as follows:
+
+// Take any positive integer n. If n is even, divide n by 2 to get n / 2. If n is odd, multiply n by 3 and add 1 to get 3n + 1. Repeat the process indefinitely. The conjecture states that no matter which number you start with, you will always reach 1 eventually.
+
+// Given a number n, return the number of steps required to reach 1.
+
+// Examples
+// Starting with n = 12, the steps would be as follows:
+
+// 12
+// 6
+// 3
+// 10
+// 5
+// 16
+// 8
+// 4
+// 2
+// 1
+
+// Resulting in 9 steps. So for input n = 12, the return value would be 9.
+
+function collatzConjecture(n) {
+  var numSteps = 0;
+  while (n > 1) {
+    if (n % 2 === 0) {
+      n = n / 2;
+    } else {
+      n = 3 * n + 1;
+    }
+    numSteps++;
+  }
+  return numSteps;
+}
+// console.log(collatzConjecture(19));
+
+
+
+// A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
+
+// Find the largest palindrome made from the product of two 3-digit numbers.
+
+var largestPalindrome = 0;
+var numberOne = 101;
+while (numberOne < 1000) {
+  var numberTwo = 100;
+  while (numberTwo < 1000) {
+    var product = numberOne * numberTwo;
+    if (product === parseInt(product.toString().split('').reverse().join(''))) {
+      largestPalindrome = product;
+    }
+    numberTwo++;
+  }
+  numberOne++;
+
+}
+console.log(largestPalindrome);
+
+
+
