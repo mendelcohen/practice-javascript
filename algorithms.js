19877ee065ee437b85c5bbf5f8f869a20407d6df
Mendel Cohen <mmhakohen@gmail.com>
2021-04-26 10:42:59 -0400

    solves order the whole menu

--- algorithms.js
+++ algorithms.js
@@ -791,4 +791,24 @@ function voteCount(array) {
   });
   return votes;
 }
-console.log(voteCount(["Dewey", "Truman", "Dewey", "Dewey", "Truman", "Truman", "Dewey", "Truman", "Truman", "Dewey", "Dewey"]));
+// console.log(voteCount(["Dewey", "Truman", "Dewey", "Dewey", "Truman", "Truman", "Dewey", "Truman", "Truman", "Dewey", "Dewey"]));
+
+
+
+// Given a hash, where the keys are strings representing food items, and the values are numbers representing the price of each food, return the amount someone would pay if they'd order one of each food from the entire menu.
+
+// Input: {"hot dog" => 2, "hamburger" => 3, "steak sandwich" => 5, "fries" => 1, "cole slaw" => 1, "soda" => 2}
+
+// Output: 14
+
+// Explanation: If someone would order one of everything on the menu, they'd pay a total of 14 (the sum of all the hash's values).
+
+const orderPrice = object => {
+  let totalPrice = 0;
+  Object.entries(object).forEach(entry => {
+    const [key, value] = entry;
+    totalPrice += entry[1];
+  });
+  return totalPrice;
+};
+console.log(orderPrice({"hot dog": 2, "hamburger":3, "steak sandwich": 5, "fries": 1, "cole slaw": 1, "soda": 2, "grilled vegetables": 4}));
