cf1ed6f1ff6456c2fc76aa37c59d904531407eb1
Mendel Cohen <mmhakohen@gmail.com>
2021-04-27 23:33:10 -0400

    solves popular posts in two ways

--- algorithms.js
+++ algorithms.js
@@ -848,5 +848,55 @@ const dnaToRna = string => {
   });
   return rna;
 };
-console.log(dnaToRna("ACGTGGTCTTAA"));
-console.log(dnaToRna("ACGTGGTCTTAAB"));
+// console.log(dnaToRna("ACGTGGTCTTAA"));
+// console.log(dnaToRna("ACGTGGTCTTAAB"));
+
+
+
+// Given an array of hashes that represent a list of social media posts, return a new array that only contains the posts that have at least 1000 likes.
+
+// Input: [
+// {title: 'Me Eating Pizza', submitted_by: "Joelle P.", likes: 1549},
+// {title: 'i never knew how cool i was until now', submitted_by: "Lyndon Johnson", likes: 3},
+// {title: 'best selfie evar!!!', submitted_by: "Patti Q.", likes: 1092},
+// {title: 'Mondays are the worst', submitted_by: "Aunty Em", likes: 644}
+// ]
+
+// Output: [
+// {title: 'Me Eating Pizza', submitted_by: "Joelle P.", likes: 1549},
+// {title: 'best selfie evar!!!', submitted_by: "Patti Q.", likes: 1092},
+// ]
+
+function thousandLikes(array) {
+  var newArray = [];
+  array.forEach(post => {
+    if (post["likes"] >= 1000) {
+      newArray.push(post);
+    }
+  });
+  return newArray;
+}
+// console.log(thousandLikes([
+//   {title: 'Me Eating Pizza', submitted_by: "Joelle P.", likes: 1549},
+//   {title: 'i never knew how cool i was until now', submitted_by: "Lyndon Johnson", likes: 3},
+//   {title: 'best selfie evar!!!', submitted_by: "Patti Q.", likes: 1092},
+//   {title: 'Mondays are the worst', submitted_by: "Aunty Em", likes: 644}
+// ]));
+
+
+const oneThousandLikes = array => {
+  let index = 0;
+  while (index < array.length) {
+    if (array[index]["likes"] < 1000) {
+      array.splice(index, 1);
+    }
+    index++;
+  }
+  return array;
+};
+// console.log(oneThousandLikes([
+//   {title: 'Me Eating Pizza', submitted_by: "Joelle P.", likes: 1549},
+//   {title: 'i never knew how cool i was until now', submitted_by: "Lyndon Johnson", likes: 3},
+//   {title: 'best selfie evar!!!', submitted_by: "Patti Q.", likes: 1092},
+//   {title: 'Mondays are the worst', submitted_by: "Aunty Em", likes: 644}
+// ]));
