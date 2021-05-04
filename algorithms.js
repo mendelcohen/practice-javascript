a650dfbf175dc180fc877a9c2668a58327d8b869
Mendel Cohen <mmhakohen@gmail.com>
2021-05-04 12:58:58 -0400

    solves etl 2

--- algorithms.js
+++ algorithms.js
@@ -1039,4 +1039,102 @@ const reverseObject = object => {
   });
   return reversedObject;
 };
-console.log(reverseObject({"a": 1, "b": 2, "c": 3}));
+// console.log(reverseObject({"a": 1, "b": 2, "c": 3}));
+
+
+
+// You are given a hash in format #A, and you are to return the same data as a hash using format #B, as specified below:
+
+// Input:
+
+// {
+// 1 => ["A", "E", "I", "O", "U"]
+// }
+
+// Output:
+
+// {
+// 'a' => 1,
+// 'e' => 1,
+// 'i' => 1,
+// 'o' => 1,
+// 'u' => 1
+// }
+// Input:
+
+// {
+// 1 => ["A", "E"],
+// 2 => ["D", "G"]
+// }
+
+// Output:
+
+// {
+// 'a' => 1,
+// 'd' => 2,
+// 'e' => 1,
+// 'g' => 2
+// }
+
+// Input:
+
+// 1 => ["A", "E", "I", "O", "U", "L", "N", "R", "S", "T"],
+// 2 => ["D", "G"],
+// 3 => ["B", "C", "M", "P"],
+// 4 => ["F", "H", "V", "W", "Y"],
+// 5 => ["K"],
+// 8 => ["J", "X"],
+// 10 => ["Q", "Z"]
+// }
+
+// Output:
+
+// {
+// 'a' => 1,
+// 'b' => 3,
+// 'c' => 3,
+// 'd' => 2,
+// 'e' => 1,
+// 'f' => 4,
+// 'g' => 2,
+// 'h' => 4,
+// 'i' => 1,
+// 'j' => 8,
+// 'k' => 5,
+// 'l' => 1,
+// 'm' => 3,
+// 'n' => 1,
+// 'o' => 1,
+// 'p' => 3,
+// 'q' => 10,
+// 'r' => 1,
+// 's' => 1,
+// 't' => 1,
+// 'u' => 1,
+// 'v' => 4,
+// 'w' => 4,
+// 'x' => 8,
+// 'y' => 4,
+// 'z' => 10
+// }
+
+function valuesToKeys(object) {
+  var newObject = {};
+  Object.entries(object).forEach(array => {
+    var [k, v] = array;
+    for (let i = 0; i < v.length; i++) {
+      newObject[v[i]] = k;
+    }
+  });
+  newObject = Object.keys(newObject).sort().reduce(
+    (obj, key) => {
+      obj[key] = newObject[key];
+      return obj;
+    },
+    {}
+  );
+  return newObject;
+}
+console.log(valuesToKeys({1: ["A", "E", "I", "O", "U"]}));
+console.log(valuesToKeys({1: ["A", "E"], 2: ["D", "G"]}));
+console.log(valuesToKeys({1: ["A", "E", "I", "O", "U", "L", "N", "R", "S", "T"], 2: ["D", "G"], 3: ["B", "C", "M", "P"], 4: ["F", "H", "V", "W", "Y"], 5: ["K"], 8: ["J", "X"], 10: ["Q", "Z"]}));
