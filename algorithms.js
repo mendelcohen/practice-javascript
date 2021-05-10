303155fa10bb773d6fd8c5d4a871268e7d04fe06
Mendel Cohen <mmhakohen@gmail.com>
2021-05-10 12:01:32 -0400

    solves missing letter 2 ways

--- algorithms.js
+++ algorithms.js
@@ -1379,8 +1379,8 @@ function subset(array1, array2) {
   }
   return true;
 }
-console.log(subset([1, 2, 3, 4, 5, 6], [6, 3, 2]));
-console.log(subset([1, 2, 3, 4, 5, 6], [6, 3, 7]));
+// console.log(subset([1, 2, 3, 4, 5, 6], [6, 3, 2]));
+// console.log(subset([1, 2, 3, 4, 5, 6], [6, 3, 7]));
 
 
 
@@ -1401,4 +1401,44 @@ const duplicate = array => {
     } 
   }
 };
-console.log(duplicate([5, 2, 9, 7, 2, 6]));
+// console.log(duplicate([5, 2, 9, 7, 2, 6]));
+
+
+
+// A given string contains all the letters from the alphabet except for one. Return the missing letter.
+
+// NOTE: You must accomplish this in O(n) time. This is also known as linear time.
+
+// Input: “The quick brown box jumps over a lazy dog”
+// Output: “f”
+
+const missingLetter = str => {
+  let letter = "";
+  let abc = {"a": 0, "b": 0, "c": 0, "d": 0, "e": 0, "f": 0, "g": 0, "h": 0, "i": 0, "j": 0, "k": 0, "l": 0, "m": 0, "n": 0, "o": 0, "p": 0, "q": 0, "r": 0, "s": 0, "t": 0, "u": 0, "v": 0, "w": 0, "x": 0, "y": 0, "z": 0};
+  for (let i = 0; i < str.length; i++) {
+    let j = str[i].toLowerCase();
+    if (j in abc) {
+      delete abc[j];
+    }
+  }
+  let array = Object.keys(abc);
+  letter = array[0];
+  return letter;
+};
+console.log(missingLetter("The quick brown box jumps over a lazy dog"));
+
+const missingLet = str => {
+  let letter = "";
+  let abc = {"a": 0, "b": 0, "c": 0, "d": 0, "e": 0, "f": 0, "g": 0, "h": 0, "i": 0, "j": 0, "k": 0, "l": 0, "m": 0, "n": 0, "o": 0, "p": 0, "q": 0, "r": 0, "s": 0, "t": 0, "u": 0, "v": 0, "w": 0, "x": 0, "y": 0, "z": 0};
+  for (let i = 0; i < str.length; i++) {
+    let j = str[i].toLowerCase();
+    if (j in abc) {
+      delete abc[j];
+    }
+  }
+  for ( let property in abc) {
+    letter = property;
+  }
+  return letter;
+};
+console.log(missingLet("The quick brown box jumps over a lazy dog"));
