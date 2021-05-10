56b5c501f2d6585173def4536252ddd5e5b4cc9f
Mendel Cohen <mmhakohen@gmail.com>
2021-05-10 12:16:31 -0400

    solves missing letter third way

--- algorithms.js
+++ algorithms.js
@@ -1442,3 +1442,17 @@ const missingLet = str => {
   return letter;
 };
 console.log(missingLet("The quick brown box jumps over a lazy dog"));
+
+const missLetter = str => {
+  let abc = {"a": 0, "b": 0, "c": 0, "d": 0, "e": 0, "f": 0, "g": 0, "h": 0, "i": 0, "j": 0, "k": 0, "l": 0, "m": 0, "n": 0, "o": 0, "p": 0, "q": 0, "r": 0, "s": 0, "t": 0, "u": 0, "v": 0, "w": 0, "x": 0, "y": 0, "z": 0};
+  for (let i = 0; i < str.length; i++) {
+    let j = str[i].toLowerCase();
+    abc[j] += 1;
+  }
+  for (var prop in abc) {
+    if (abc[prop] === 0) {
+      return prop;
+    }
+  }
+};
+console.log(missLetter("The quick brown box jumps over a lazy dog"));
